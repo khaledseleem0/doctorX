35cbf68aa763964f21f09a2241c6b7bdcc8bcfb4
khaled seleem <73780446+khaledseleem0@users.noreply.github.com>
2023-01-17 22:17:25 +0200

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -25,11 +25,13 @@ function Navbar() {
               <a href="#home">Home</a>
             </li>
             <li>
-              <a href="#about">Company</a>
-            </li>
-            <li>
               <a href="#services">Services</a>
             </li>
+
+            <li>
+              <a href="#about">About</a>
+            </li>
+
             <li>
               <a href="#contact">Contact</a>
             </li>
